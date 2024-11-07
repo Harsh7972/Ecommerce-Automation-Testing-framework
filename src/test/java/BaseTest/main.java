0a996e54f8cea09e65000745c51d13a289691496
Harsh7972 <kapseharshal8@gmail.com>
2024-11-07 22:43:34 +0530

    Third commit

--- src/test/java/BaseTest/main.java
+++ src/test/java/BaseTest/main.java
@@ -4,4 +4,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("new file added");
     }
+
+    public void printname(){
+        System.out.println("Harshal Kapse");
+    }
 }
